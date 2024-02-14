d639dba0c1d7071e8ddefb60aa2c752fceebed6d
rayan <rayanzyad99@gmail.com>
2024-02-14 15:05:37 +0000

    Start

--- src/main/java/Security/AuthorizationFilter.java
+++ src/main/java/Security/AuthorizationFilter.java
@@ -1,5 +1,20 @@
 package Security;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
 // Using the filter to check if the request to a web service endpoint does contain a JWT token and if the provided
 // JWT token has been signed with the correct token secret
-public class AuthorizationFilter {
+@Component
+public class AuthorizationFilter extends AbstractGatewayFilterFactory<AuthorizationFilter.Config> {
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return null;
+    }
+
+    public static class Config {
+
+    }
 }
