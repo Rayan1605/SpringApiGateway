73dff3ac6ccd7dbf69918b64b1491688616915f9
rayan <rayanzyad99@gmail.com>
2024-02-14 15:02:00 +0000

    Start

--- src/main/java/Security/AuthorizationFilter.java
+++ src/main/java/Security/AuthorizationFilter.java
@@ -1,4 +1,5 @@
 package Security;
-
+// Using the filter to check if the request to a web service endpoint does contain a JWT token and if the provided
+// JWT token has been signed with the correct token secret
 public class AuthorizationFilter {
 }
