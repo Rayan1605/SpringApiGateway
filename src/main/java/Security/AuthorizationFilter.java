a60711bb7c07ce155a1d5713c92123b8225a7ff1
rayan <rayanzyad99@gmail.com>
2024-02-14 15:21:18 +0000

    Start

--- src/main/java/Security/AuthorizationFilter.java
+++ src/main/java/Security/AuthorizationFilter.java
@@ -4,7 +4,10 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 // Using the filter to check if the request to a web service endpoint does contain a JWT token and if the provided
 // JWT token has been signed with the correct token secret
@@ -22,6 +25,13 @@ public class AuthorizationFilter extends AbstractGatewayFilterFactory<Authorizat
             }
             return chain.filter(exchange);
         };
+    }
+
+    private Mono<Void> OneError(ServerWebExchange exchange, String s, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        return response.setComplete();
     }
 
     public static class Config {
